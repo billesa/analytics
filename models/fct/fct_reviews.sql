a74761ef1da7d7323b6c54cf508fa97af4898cd6
billesand@gmail.com <billesand@gmail.com>
2022-09-09 16:13:04 +0000

    fredag

--- models/fct/fct_reviews.sql
+++ models/fct/fct_reviews.sql
@@ -14,3 +14,9 @@ WITH src_reviews AS (
 SELECT *
 FROM src_reviews
 WHERE review_text IS NOT NULL
+
+{% if is_incremental() %}
+    AND review_date > (
+        select max(review_date)
+        from {{ this }})
+{% endif %}
